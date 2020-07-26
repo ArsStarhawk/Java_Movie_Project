be47ca41fc1a68bb163b224840ac5a95d962991e
scullyjms <scullyjms@gmail.com>
2020-07-25 20:41:32 -0400

    Setting up outline for the controller

--- src/MovieRentalController.java
+++ src/MovieRentalController.java
@@ -1,11 +1,20 @@
 /**
  * Program Name: MovieRentalController.java
- * Purpose:
- * Coder: 
+ * Purpose: Manages the MovieRentalModel and the MovieRentalView classes
+ * Coder: James Scully
  * Date: Jul 14, 2020
  */
 
 public class MovieRentalController
 {
-
+	// Properties
+	private MovieRentalModel model;
+	private MovieRentalView view;
+	
+	// Constructor
+	public MovieRentalController(){
+		this.model = new MovieRentalModel();
+		this.view = new MovieRentalView();
+	}
+	
 }
