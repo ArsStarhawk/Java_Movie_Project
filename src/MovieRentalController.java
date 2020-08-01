03dccd3f0f9d08f969ec2efc16b0f38a4168dccf
Sion Young <sioniunn@gmail.com>
2020-07-31 22:56:07 -0400

    Finish generate report view

--- src/MovieRentalController.java
+++ src/MovieRentalController.java
@@ -28,25 +28,33 @@ public class MovieRentalController
   private class addActorListener implements ActionListener
   {
     @Override
-    public void actionPerformed(ActionEvent e) {
-      try {
-        System.out.println("abc");
+    public void actionPerformed(ActionEvent e)
+    {
+      try
+      {
         String fName = theView.tflFirstname.getText();
         String lName = theView.tflLastname.getText();
-        if (fName.isEmpty()) {
-          createPopupDialog("Error", "Please enter first name");
+        if (fName.isEmpty())
+        {
+          createPopupDialog("Error", "Please enter actor's first name.");
           theView.tflFirstname.requestFocus();
-        } else if (lName.isEmpty()) {
-          createPopupDialog("Error", "Please enter last name");
+        }
+        else if (lName.isEmpty())
+        {
+          createPopupDialog("Error", "Please enter actor's last name.");
           theView.tflLastname.requestFocus();
-        } else {
+        }
+        else
+        {
           String stmt = "INSERT INTO Actor (first_name, last_name) " +
               "VALUES ( '" + fName + "', '" + lName + "');";
           theModel.addActor(stmt);
           createPopupDialog("Database updated",
               "Actor " + fName + " " + lName + " is added to the database.");
         }
-        } catch(SQLException exception) {
+      }
+      catch(SQLException exception)
+      {
           exception.printStackTrace();
       }
     }
@@ -55,11 +63,15 @@ public class MovieRentalController
   private void populateCategoryDropdownForGenerateReportPane()
   {
     ResultSet categories = theModel.getAllCategories();
-    try{
-      while(categories.next()){
+    try
+    {
+      while(categories.next())
+      {
         theView.cbCategory.addItem(categories.getString("name"));
       }
-    } catch(SQLException ex){
+    }
+    catch(SQLException ex)
+    {
       createPopupDialog("Error", "Not able to load categories");
       System.out.println(ex.getMessage());
     }
@@ -69,9 +81,11 @@ public class MovieRentalController
   {
 
     ResultSet stores = theModel.getAllStores();
-    theView.cbStore.addItem("All Stores");
-    try{
-      while(stores.next()){
+    theView.cbStore.addItem("All Stores      ");
+    try
+    {
+      while(stores.next())
+      {
         theView.cbStore.addItem(stores.getString("store_id"));
       }
     } catch(SQLException ex){
@@ -80,7 +94,8 @@ public class MovieRentalController
     }
   }
 
-  private void createPopupDialog(String dialogTitle, String msg) {
+  private void createPopupDialog(String dialogTitle, String msg)
+  {
     JOptionPane pane = new JOptionPane(msg);
     JDialog dialog = pane.createDialog(dialogTitle);
     dialog.show();
