d4cd48c6926724f972aa13aa7b74b8268c597d28
Sion Young <sioniunn@gmail.com>
2020-08-06 21:52:23 -0400

    remove extrea pop-up dialog

--- src/AddFilmListener.java
+++ src/AddFilmListener.java
@@ -65,97 +65,93 @@ public class AddFilmListener implements ActionListener
     boolean hasBehindScenes = view.behindScenes.isSelected()? true: false;
 
     boolean isValidInput = getInputAndValidate();
-
-    if(yearEntered < 2015)
-    {
-      rentalRateFinal = 0.99;
-    }
-    else
-    {
-      rentalRateFinal = 4.99;
-    }
-
-    //Getting SET of special features
-    if(hasTrailers)
-      specialFeatures.add("Trailers");
-    if(hasCommentary)
-      specialFeatures.add("Commentaries");
-    if(hasDeletedScenes)
-      specialFeatures.add("Deleted Scenes");
-    if(hasBehindScenes)
-      specialFeatures.add("Behind the Scenes");
-
-    specialFeatureStr = String.join(",", specialFeatures);
-
-    //insert into film statement
-    String insertStatement = String.format("INSERT INTO film "
-            + "(title, description, release_year,language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features) "
-            + "VALUES(\"%s\",\"%s\",%d,%d,%d,%d,%f,%d,%f,\"%s\",'%s');"
-        , titleStr, descriptionStr, yearEntered, languageStr, origLanguageStr,rentalDurationStr,rentalRateFinal,movieLengthStr,replacementCostStr,ratingStr, specialFeatureStr);
-
-    //start of database updates
-    try
-    {
-      myConn = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/sakila?useSSL=false","root", "password");
-
-      myStmt = myConn.createStatement();
-      System.out.println(insertStatement);
-      //catch the returned int value after the insert is executed
-      int returnedValue = myStmt.executeUpdate(insertStatement);
-      String selectStatement = String.format("SELECT film_id FROM film WHERE title = \"%s\"", titleStr);
-      myRslt = myStmt.executeQuery(selectStatement);
-      myRslt.next();
-      int updatedId = myRslt.getInt(1);
-
-      //INSERTING ACTORS
-      for(String actorName : view.selectedActors)
-      {
-        String firstName = actorName.split(" ")[0];
-        String lastName = actorName.split(" ")[1];
-        String selectActorId = String.format("SELECT actor_id FROM actor WHERE first_name = \"%s\" and last_name = \"%s\";", firstName, lastName);
-        myRslt = myStmt.executeQuery(selectActorId);
-        myRslt.next();
-        int actorId = myRslt.getInt(1);
-        String insertActor = String.format("INSERT INTO film_actor (actor_id, film_id) VALUES (%d, %d);", actorId, updatedId);
-        int ret = myStmt.executeUpdate(insertActor);
+    if (isValidInput) {
+      if (yearEntered < 2015) {
+        rentalRateFinal = 0.99;
+      } else {
+        rentalRateFinal = 4.99;
       }
 
-      //Insert film_categories
-      String insertStatement2 = String.format("INSERT INTO film_category (film_id, category_id) VALUES (%d, %d);",updatedId, categoryStr);
-      System.out.println(insertStatement2);
+      //Getting SET of special features
+      if (hasTrailers)
+        specialFeatures.add("Trailers");
+      if (hasCommentary)
+        specialFeatures.add("Commentaries");
+      if (hasDeletedScenes)
+        specialFeatures.add("Deleted Scenes");
+      if (hasBehindScenes)
+        specialFeatures.add("Behind the Scenes");
 
-      int ret = myStmt.executeUpdate(insertStatement2);
+      specialFeatureStr = String.join(",", specialFeatures);
+
+      //insert into film statement
+      String insertStatement = String.format("INSERT INTO film "
+              + "(title, description, release_year,language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features) "
+              + "VALUES(\"%s\",\"%s\",%d,%d,%d,%d,%f,%d,%f,\"%s\",'%s');"
+          , titleStr, descriptionStr, yearEntered, languageStr, origLanguageStr, rentalDurationStr, rentalRateFinal, movieLengthStr, replacementCostStr, ratingStr, specialFeatureStr);
+      //start of database updates
+      try
+      {
+        myConn = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/sakila?useSSL=false","root", "password");
+
+        myStmt = myConn.createStatement();
+        System.out.println(insertStatement);
+        //catch the returned int value after the insert is executed
+        int returnedValue = myStmt.executeUpdate(insertStatement);
+        String selectStatement = String.format("SELECT film_id FROM film WHERE title = \"%s\"", titleStr);
+        myRslt = myStmt.executeQuery(selectStatement);
+        myRslt.next();
+        int updatedId = myRslt.getInt(1);
+
+        //INSERTING ACTORS
+        for(String actorName : view.selectedActors)
+        {
+          String firstName = actorName.split(" ")[0];
+          String lastName = actorName.split(" ")[1];
+          String selectActorId = String.format("SELECT actor_id FROM actor WHERE first_name = \"%s\" and last_name = \"%s\";", firstName, lastName);
+          myRslt = myStmt.executeQuery(selectActorId);
+          myRslt.next();
+          int actorId = myRslt.getInt(1);
+          String insertActor = String.format("INSERT INTO film_actor (actor_id, film_id) VALUES (%d, %d);", actorId, updatedId);
+          int ret = myStmt.executeUpdate(insertActor);
+        }
+
+        //Insert film_categories
+        String insertStatement2 = String.format("INSERT INTO film_category (film_id, category_id) VALUES (%d, %d);",updatedId, categoryStr);
+        System.out.println(insertStatement2);
+
+        int ret = myStmt.executeUpdate(insertStatement2);
 
 
-      //Step 4: output results of operation
-      JOptionPane.showMessageDialog(view.getContentPane(), titleStr + " film inserted into sakila database");
+        //Step 4: output results of operation
+        JOptionPane.showMessageDialog(view.getContentPane(), titleStr + " film inserted into sakila database");
 
-    }
-    catch(Exception ex)
-    {
-      JOptionPane.showMessageDialog(view.getContentPane(), "There was an error inserting");
-    }
-
-    //DO THE finally block!
-    finally
-    {
-      //put your clean up code here to close the objects. Standard practice is to
-      //close them in REVERSE ORDER of creation
-      try {
-        if(myRslt != null)
-          myRslt.close();
-        if(myStmt != null)
-          myStmt.close();
-        if(myConn != null)
-          myConn.close();
       }
       catch(Exception ex)
       {
-        System.out.println("Exception caught, message is " + ex.getMessage());
+        JOptionPane.showMessageDialog(view.getContentPane(), "There was an error inserting");
+      }
+
+      //DO THE finally block!
+      finally
+      {
+        //put your clean up code here to close the objects. Standard practice is to
+        //close them in REVERSE ORDER of creation
+        try {
+          if(myRslt != null)
+            myRslt.close();
+          if(myStmt != null)
+            myStmt.close();
+          if(myConn != null)
+            myConn.close();
+        }
+        catch(Exception ex)
+        {
+          System.out.println("Exception caught, message is " + ex.getMessage());
+        }
       }
     }
-
   }
 
   private boolean getInputAndValidate()
